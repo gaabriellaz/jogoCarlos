719ac5b84bd3d06d475c274e0cb24f77371ebc93
gaabriellaz <gabriella.nunes24@nave.org.br>
2025-08-07 15:35:22 -0300

    mooooodal

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello!');
+// console.log('sdasd!');
+
+const Modal = document.querySelector('dialog')
+// console.log(Modal)
+
+function openModal(){
+    Modal.showModal();
+}
+
+function CloseModal() {
+    Modal.close();
+}
